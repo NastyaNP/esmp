4ff57218d5092c8b342de61ffa34ed888a6b701d
Anastasia Polezhaeva <an.polezhaeva@list.ru>
2023-10-11 22:45:17 +0300

    Добавила форму авторизации и изменила тип авторизации

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,43 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule, inject } from '@angular/core';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { TicketInformationComponent } from './ticket-information/ticket-information.component';
 import { TicketsListComponent } from './tickets-list/tickets-list.component';
+import { LoginComponent } from './login/login.component';
+import { authGuard } from './guards/auth.guard';
+import { AuthService } from './services/auth.service';
+import { map, tap } from 'rxjs';
 
 const routes: Routes = [
   {
     path: "info",
     pathMatch: "full",
-    component: TicketInformationComponent
+    component: TicketInformationComponent,
+    canActivate: [authGuard]
   },
   {
     path: "",
     pathMatch: "full",
-    component: TicketsListComponent
+    component: TicketsListComponent,
+    canActivate: [authGuard]
   },
   {
-    path: "**",
+    path: "login",
     pathMatch: "full",
-    component: TicketsListComponent
-  },
+    component: LoginComponent,
+    canActivate: [() => {
+      const router = inject(Router);
+      return inject(AuthService).isAuthorized$.pipe(
+        map((isAuthorized: boolean) => {
+          if (!isAuthorized) {
+            return true;
+          }
+
+          const tree = router.parseUrl("/");
+          return tree;
+        }),
+      );
+    }]
+  }
 ];
 
 @NgModule({
